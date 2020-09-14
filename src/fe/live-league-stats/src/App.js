df8fb634ad57fd590ac7e8927353f1129a523a40
JosephGentile1998 <63070917+JosephGentile1998@users.noreply.github.com>
2020-09-14 12:31:59 -0400

    Feature/frontend1 (#2)
    * The empty React Material UI project was added to the fe folder.
    
    * Adding stuff back
    
    * Changes
    
    * started front end

---
+++ src/fe/live-league-stats/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+
+    </div>
+  );
+}
+
+export default App;
--- src/python/IP/venv/bin/wsdump.py
+++
@@ -1,4 +0,0 @@
-#!/Users/alex/Desktop/Test/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'websocket-client==0.57.0','wsdump.py'
-__requires__ = 'websocket-client==0.57.0'
-__import__('pkg_resources').run_script('websocket-client==0.57.0', 'wsdump.py')
